e47a1626ced8ec0074e02199f5853e2d97d43216
k-room <ronakorn.steam@hotmail.com>
2024-07-01 16:26:16 +0700

    nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,12 @@ const nextConfig = {
     images: {
         domains: ['lh3.googleusercontent.com'],
     },
+    env: {
+        NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY,
+        NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+        NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+        NEXTEMAIL_URL: process.env.NEXTEMAIL_URL,
+    }
     // experimental: {
     //     serverActions: true,
     // },
