8904b1dbb64f90c1723f3e4788c8a5c369deaeac
k-room <ronakorn.steam@hotmail.com>
2024-07-03 15:40:24 +0700

    env c

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,9 @@ const nextConfig = {
         serverActions: true,
         mutations: true,
     },
+    serverRuntimeConfig: {
+        allowedOrigins: ['https://app.nipponexpress-necl.co.th:3000', 'http://app.nipponexpress-necl.co.th:3000', 'https://app.nipponexpress-necl.co.th', 'http://app.nipponexpress-necl.co.th'],
+    },
     reactStrictMode: true,
 };
 
