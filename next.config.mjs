1ec85aa1b31731e26cdcc43833c9ba532e8c3f42
k-room <ronakorn.steam@hotmail.com>
2024-07-03 10:56:53 +0700

    Add env

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,12 @@ const nextConfig = {
         domains: ['lh3.googleusercontent.com'],
     },
     reactStrictMode: true,
+    env: {
+        NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY,
+        NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+        NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+        NEXTEMAIL_URL: process.env.NEXTEMAIL_URL
+    },
 };
 
 export default nextConfig;
