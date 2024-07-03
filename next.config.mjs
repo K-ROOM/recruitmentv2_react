6b0289465ca65cfaa4947325c9719e80d7345216
k-room <ronakorn.steam@hotmail.com>
2024-07-03 13:45:18 +0700

    ...

--- next.config.mjs
+++ next.config.mjs
@@ -4,12 +4,12 @@ const nextConfig = {
         domains: ['lh3.googleusercontent.com'],
     },
     reactStrictMode: true,
-    env: {
-        NEXT_PUBLIC_API_KEY: "http://localhost:3000",
-        NEXTAUTH_URL: "http://localhost:3001",
-        NEXTAUTH_SECRET: "rms-access-token",
-        NEXTEMAIL_URL: "http://172.16.58.231:3002/main"
-    },
+    // env: {
+    //     NEXT_PUBLIC_API_KEY: "http://localhost:3000",
+    //     NEXTAUTH_URL: "http://localhost:3001",
+    //     NEXTAUTH_SECRET: "rms-access-token",
+    //     NEXTEMAIL_URL: "http://172.16.58.231:3002/main"
+    // },
 };
 
 export default nextConfig;
