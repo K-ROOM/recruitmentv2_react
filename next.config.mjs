de2c3a64b0fb06768204a76146bbc05f848a5613
k-room <ronakorn.steam@hotmail.com>
2024-07-03 16:23:42 +0700

    ddddd

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
         mutations: true,
     },
     serverRuntimeConfig: {
-        allowedOrigins: ['https://app.nipponexpress-necl.co.th:3000', 'http://app.nipponexpress-necl.co.th:3000', 'https://app.nipponexpress-necl.co.th', 'http://app.nipponexpress-necl.co.th'],
+        allowedOrigins: ['https://10.0.0.7:3000', 'http://10.0.0.7:3000', 'https://10.0.0.7', 'http://10.0.0.7'],
     },
     reactStrictMode: true,
 };
