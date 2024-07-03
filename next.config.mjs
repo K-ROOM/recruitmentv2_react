0bbbde645ec421c8141669a5cc8903ad786d04ff
k-room <ronakorn.steam@hotmail.com>
2024-07-03 14:23:50 +0700

    env again

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
     reactStrictMode: true,
     env: {
         NEXT_PUBLIC_API_KEY: "https://app.nipponexpress-necl.co.th:3000",
-        NEXTAUTH_URL: "https://app.nipponexpress-necl.co.th:3001",
+        NEXTAUTH_URL: "http://localhost:3001",
         NEXTAUTH_SECRET: "rms-access-token",
         NEXTEMAIL_URL: "http://172.16.58.231:3002/main"
     },
